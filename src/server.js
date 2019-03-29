0bf62323750cde5e63fe428a236802512fb10cee
neeraj-dixit <neeraj.dixit@nutanix.com>
2019-03-29 20:41:18 +0530

    added changes

--- src/server.js
+++ src/server.js
@@ -5,17 +5,29 @@
  import express from 'express';
 import expressGraphql from 'express-graphql';
 import { buildSchema } from 'graphql';
+import { getCourse, getCourses } from './courses';
 import { SERVERPORT } from './config';
 
 // GraphQL schema
 var schema = buildSchema(`
     type Query {
-        message: String
+      course(id: Int!): Course,
+      courses(topic: String): [Course]
+    },
+    type Course {
+      id: Int
+      title: String
+      author: String
+      description: String
+      topic: String
+      url: String
     }
 `);
+
 // Root resolver
 var root = {
-    message: () => 'Hello World!'
+  course: getCourse,
+  courses: getCourses,
 };
 
 const graphqlApp = express();
